85e946a07caa909bbdfe79c8b24fee386a9e592c
Alba <alba__22@outlook.com>
2024-01-13 14:56:52 -0300

    feat: add endpoint for getting all tasks

--- functions/src/api/tasks.ts
+++ functions/src/api/tasks.ts
@@ -63,3 +63,39 @@ export const createTask = async (
     });
   }
 };
+
+export const getAllTasks = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    verifyUserId(req, res);
+
+    const db = admin.firestore();
+    const userId = req.query["id"] as string;
+
+    const tasksSnapshot = await db
+      .collection(usersCollection)
+      .doc(userId)
+      .collection(tasksCollection)
+      .get();
+
+    const tasks: Task[] = tasksSnapshot.docs.map((e) => {
+      return {
+        id: e.id,
+        title: e.data().title,
+        category: e.data().category,
+        completed: e.data().completed,
+        description: e.data().description,
+        date: (e.data().date as Timestamp).toDate().toISOString(),
+      };
+    });
+
+    res.status(200).json(tasks);
+  } catch (exception) {
+    logger.error(exception, { structuredData: true });
+    res.status(500).json({
+      message: "Ocorreu um erro inesperado!",
+    });
+  }
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
-import { createTask } from "./api/tasks";
+import { createTask, getAllTasks } from "./api/tasks";
 import { getUserId } from "./api/users";
 
 admin.initializeApp();
@@ -9,6 +9,7 @@ admin.initializeApp();
 export const app = express();
 
 app.post("/tasks", createTask);
+app.get("/tasks", getAllTasks);
 app.get("/id", getUserId);
 
 export const api = functions.https.onRequest(app);
