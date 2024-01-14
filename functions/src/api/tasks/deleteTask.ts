69589b9830e120200224eeb202b93aca4cf3bdee
Alba <alba__22@outlook.com>
2024-01-14 15:50:21 -0300

    feat: add endpoint for deleting a task

---
+++ functions/src/api/tasks/deleteTask.ts
@@ -0,0 +1,46 @@
+import * as express from "express";
+import * as admin from "firebase-admin";
+import { verifyUserId } from "../../utils/verifyUserId";
+import { validateRequestBody } from "../../utils/validateRequestBody";
+import { tasksCollection, usersCollection } from "../../utils/constants";
+import { logger } from "firebase-functions/v1";
+
+interface DeleteTaskInput {
+  id: string;
+}
+
+const deleteTaskValidator: Record<
+  keyof DeleteTaskInput,
+  (value: any) => boolean
+> = {
+  id: (value) => typeof value === "string",
+};
+
+export const deleteTask = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    verifyUserId(req, res);
+    validateRequestBody(req, res, deleteTaskValidator);
+
+    const userId = req.query.id as string;
+    const taskId = req.body.id as string;
+
+    const db = admin.firestore();
+
+    await db
+      .collection(usersCollection)
+      .doc(userId)
+      .collection(tasksCollection)
+      .doc(taskId)
+      .delete();
+
+    res.status(200).json();
+  } catch (exception) {
+    logger.error(exception, { structuredData: true });
+    res.status(500).json({
+      message: "Ocorreu um erro inesperado!",
+    });
+  }
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ import { createTask } from "./api/tasks/createTask";
 import { getAllTasks } from "./api/tasks/getAllTasks";
 import { getUserId } from "./api/users/getUserId";
 import { editTask } from "./api/tasks/editTask";
+import { deleteTask } from "./api/tasks/deleteTask";
 
 admin.initializeApp();
 admin.firestore().settings({ ignoreUndefinedProperties: true });
@@ -14,6 +15,7 @@ export const app = express();
 app.post("/tasks", createTask);
 app.get("/tasks", getAllTasks);
 app.put("/tasks", editTask);
+app.delete("/tasks", deleteTask);
 app.get("/id", getUserId);
 
 export const api = functions.https.onRequest(app);
